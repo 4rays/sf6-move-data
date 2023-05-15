e6fa2a5034749cbcd40e958cd39a56c81ca6f3a9
Reda Lemeden <git@redalemeden.com>
2023-05-15 13:04:08 +0200

    Add schema validation

---
+++ validate.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import json
+import os
+import jsonschema
+
+def validate_json(json_data):
+    """Validate JSON data against schema"""
+    # Load schema
+    schema_path = os.path.join(os.path.dirname(__file__), 'schema.json')
+    with open(schema_path) as schema_file:
+        schema = json.load(schema_file)
+
+    # Validate json data
+    try:
+        jsonschema.validate(json_data, schema)
+    except jsonschema.exceptions.ValidationError as err:
+        print(err)
+        return False
+    return True
+
+# Load all character moves
+moves_path = os.path.join(os.path.dirname(__file__), 'moves')
+
+for filename in os.listdir(moves_path):
+    if filename.endswith('.json'):
+        with open(os.path.join(moves_path, filename)) as move_file:
+            move = json.load(move_file)
+            print('Validating: {}'.format(filename))
+            if not validate_json(move):
+                print('Invalid move: {}'.format(filename))
+                exit(1)
