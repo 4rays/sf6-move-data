6a8e794c9013b94b4bb0ef3bc3cec6deed213338
Reda Lemeden <git@redalemeden.com>
2023-05-15 14:50:56 +0200

    Add JSOn validation

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sf6_move_data',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'jsonschema'
+    ]
+)
